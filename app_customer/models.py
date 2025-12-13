3562c6c5bb32f9d2229db2bedabbefcac9f290de
Iman Datta <dattaiman56@gmail.com>
2025-12-13 17:05:49 +0530

    register_customer

--- app_customer/models.py
+++ app_customer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    department = models.CharField(max_length=50, default='Developer')
+    created_up = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name 
--- app_customer/views.py
+++ app_customer/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Customer
+
+@api_view(['POST'])
+def register_customer(request: Request) -> Response:
+    name = request.data.get('name')
+    address = request.data.get('address')
+    department = request.data.get('department')
+
+    if not all([name, address, department]):
+        return Response({'error': 'All fields are required'}, status=400)
+    else:
+        customer = Customer.objects.create(name=name, address=address, department=department)
+
+        response_data = {
+            'message': 'Customer created successfully',
+            'customer': {
+                'id': customer.id,
+                'name': customer.name,
+                'department': customer.department
+            }
+        }
+        return Response(response_data, status=201)
