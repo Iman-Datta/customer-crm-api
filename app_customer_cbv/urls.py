fac373ba24e403e5f129ade3e2517dedbbd89448
Iman Datta <dattaiman56@gmail.com>
2025-12-27 22:41:03 +0530

    just

--- app_customer_cbv/urls.py
+++ app_customer_cbv/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('display/', DisplayCustomerView.as_view(), name='display-customer'),
     path('updatePatch/<int:pk>/', UpdateCustomerPatchView.as_view(),name='updatePatch-customer'),
     path('delete_apiURL/<int:pk>/', DeleteCustomerViewUrl.as_view(), name='delete-customer'),
-    path('delete_api/', DeleteCustomerView.as_view(), name='delete-customer'),
+    path('delete_api/', DeleteCustomerView.as_view(), name='delete-xcustomer'),
     path('updatePut/<int:pk>/', UpdateCustomerPutView.as_view(),name='updatePut-customer'),
 ]
